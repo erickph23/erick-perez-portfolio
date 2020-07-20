2a1c6d354ec0441731c9c4fe4f4808b4c0fbee29
veroca88 <veronicadonoso1506@gmail.com>
2020-07-19 21:46:10 -0400

    Updates

---
+++ precache-manifest.56f9230c4da285235ccb4566ccaa5588.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2766d4bfa28ee1d41d3d3b98b0ed561c",
+    "url": "/erick-perez-portfolio/index.html"
+  },
+  {
+    "revision": "cf875f3dabe72e80c2a7",
+    "url": "/erick-perez-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "319567aafbd74e018cce",
+    "url": "/erick-perez-portfolio/static/css/main.ab05bd1e.chunk.css"
+  },
+  {
+    "revision": "cf875f3dabe72e80c2a7",
+    "url": "/erick-perez-portfolio/static/js/2.1a6e23ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/erick-perez-portfolio/static/js/2.1a6e23ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319567aafbd74e018cce",
+    "url": "/erick-perez-portfolio/static/js/main.1be05fb7.chunk.js"
+  },
+  {
+    "revision": "ec6c01b7c0baf450919f",
+    "url": "/erick-perez-portfolio/static/js/runtime-main.e606e9bc.js"
+  },
+  {
+    "revision": "22dd5272a5f3057761b5e44658c7a1d4",
+    "url": "/erick-perez-portfolio/static/media/bg-photo.22dd5272.png"
+  }
+]);
