b8ba80ce43c11e159b168a56164258286b9eff8c
veroca88 <veronicadonoso1506@gmail.com>
2020-07-23 13:18:20 -0400

    Updates

--- precache-manifest.c5cdc455ee418b78cc8df29fd6a54e54.js
+++ precache-manifest.c5cdc455ee418b78cc8df29fd6a54e54.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d221616148fe9071d0e3fa0984372053",
+    "revision": "afee11d3db4a2f7b6067fac71aebd16d",
     "url": "/erick-perez-portfolio/index.html"
   },
   {
-    "revision": "d64a0117ca6d659a3931",
+    "revision": "b012e306d9de517aa1a6",
     "url": "/erick-perez-portfolio/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "8600751c5d08588acced",
-    "url": "/erick-perez-portfolio/static/css/main.c26b67d9.chunk.css"
+    "revision": "bae0ca701abd90e6fc96",
+    "url": "/erick-perez-portfolio/static/css/main.1121c3c4.chunk.css"
   },
   {
-    "revision": "d64a0117ca6d659a3931",
-    "url": "/erick-perez-portfolio/static/js/2.13f5aec8.chunk.js"
+    "revision": "b012e306d9de517aa1a6",
+    "url": "/erick-perez-portfolio/static/js/2.5084a993.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/erick-perez-portfolio/static/js/2.13f5aec8.chunk.js.LICENSE.txt"
+    "url": "/erick-perez-portfolio/static/js/2.5084a993.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8600751c5d08588acced",
-    "url": "/erick-perez-portfolio/static/js/main.efbffb8e.chunk.js"
+    "revision": "bae0ca701abd90e6fc96",
+    "url": "/erick-perez-portfolio/static/js/main.0835281f.chunk.js"
   },
   {
     "revision": "ec6c01b7c0baf450919f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/erick-perez-portfolio/precache-manifest.f39934870ec93359c200976eb59d51df.js"
+  "/erick-perez-portfolio/precache-manifest.c5cdc455ee418b78cc8df29fd6a54e54.js"
 );
 
 self.addEventListener('message', (event) => {
